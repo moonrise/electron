0b77be1226179be8d939588cbe7622356e20f20e
bjeong <bjeong@tibco.com>
2018-01-23 10:59:22 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./renderer.js",
+    output: {
+        filename: 'app.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
